07753e284e051408f5761224a1b0cfc94f6b4aca
abdullahrashid <abdullahrashid1017@gmail.com>
2026-01-29 21:05:00 -0500

    added the printing using slice notation

--- lab3_abdullahrashid_replace.py
+++ lab3_abdullahrashid_replace.py
@@ -1,3 +1,7 @@
 from lab3_abdullahrashid_add import camping_items
-# Replace 2 items in the list
-camping_items[2] = "headlamp"  # Replacing "flashlight"
+print(camping_items)
+# Replace 1 items in the list
+camping_items[2] = "binoculars"  # Replacing "flashlight"
+print(camping_items[0:2])
+print(camping_items[2])
+print(camping_items[3:15])
