4d99e01e4ff447aecd923eab8ebca22a3ab1b961
abdullahrashid <abdullahrashid1017@gmail.com>
2026-01-29 21:30:52 -0500

    i made new file and i had written how remove item in my campings item list

---
+++ lab3_abdullahrashid_remove.py
@@ -0,0 +1,5 @@
+from lab3_abdullahrashid_replace import camping_items
+camping_items.remove("binoculars")
+print(camping_items)
+my_var=len(camping_items)
+print(f"total number of items: {my_var}")
--- lab3_abdullahrashid_replace.py
+++ lab3_abdullahrashid_replace.py
@@ -1,5 +1,4 @@
 from lab3_abdullahrashid_add import camping_items
-print(camping_items)
 # Replace 1 items in the list
 camping_items[2] = "binoculars"  # Replacing "flashlight"
 print(camping_items[0:2])
